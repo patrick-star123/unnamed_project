02e9ac9f85eddfa0930416e93de41e321642adfb
patrick-star123 <2536199829@qq.com>
2020-06-10 14:42:10 +0800

    测试

---
+++ unnamed_project/src/main/java/com/project/UnnamedApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnnamedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnnamedApplication.class, args);
+	}
+
+}
